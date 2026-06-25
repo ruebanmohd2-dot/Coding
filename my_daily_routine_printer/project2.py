3a2fa41b97cdeacaaedda93de4dc5c900ab388c5
ruebanmohd2-dot <rueban.mohd2@gmail.com>
2026-06-25 16:33:01 +0300

    HI

---
+++ my_daily_routine_printer/project2.py
@@ -0,0 +1,11 @@
+
+Friend1 = "July 4 2014"
+print("Friend1's Bitday is on", Friend1)
+Friend2 = "January 17 2013"
+print("Friend2's Bitday is on", Friend2)
+Friend3 = "October 6 2014"
+print("Friend3's Bitday is on", Friend3)
+Friend4 = "May 4 2014"
+print("Friend4's Bitday is on", Friend4)
+Friend5 = "April 24 2014"
+print("Friend5's Bitday is on", Friend5)
