48b645442a0200bc4dbce6c3814aa781a07042b0
ruebanmohd2-dot <rueban.mohd2@gmail.com>
2026-07-10 16:30:37 +0300

    hi

--- Projects/project10.py
+++ Projects/project10.py
@@ -1,4 +1,6 @@
-b = int(input("Enter A base:"))
-p = int(input("Enter A power:"))
-
-print(b**p)
+base = int(input("Enter the base number: "))
+exponent = int(input("Enter the exponent (power): "))
+result = 1
+for i in range(exponent):
+    result = result * base
+print(f"{base} raised to  {exponent} is: {result}")
