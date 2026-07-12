e4e49b79db3f35c1ced1a4eb063b6229c914fa88
ruebanmohd2-dot <rueban.mohd2@gmail.com>
2026-07-12 16:38:55 +0300

    hi

---
+++ Projects/project13.py
@@ -0,0 +1,10 @@
+
+n = int(input("Enter a number for the number of rows: "))
+for i in range(n):
+    for j in range(n - 1 - i):
+        print(" ", end="")
+
+    for k in range(i + 1):
+        print("*", end="")
+
+    print()
